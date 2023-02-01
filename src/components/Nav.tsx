134b49e03be625b7ad83858d0fe235eb57a22b1a
Vlado Simic <vladosimic525@gmail.com>
2023-02-01 16:25:38 +0100

    First Commit

---
+++ src/components/Nav.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <div>
+      <div>
+        <ul>
+          <li>About</li>
+          <li>Skills</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
