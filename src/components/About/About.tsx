06241ad03cf7ccec7a4d4956ee7131ec32d762ab
Vlado Simic <vladosimic525@gmail.com>
2023-02-01 16:52:20 +0100

    Changed Some Text In About Component

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -6,12 +6,12 @@ const About = () => {
       <h2>About Me</h2>
       <div>
         <p>
-          I am a 36-year-old Croatian living in Bosnia and Herzegovina with 4
-          years of studying a web development. I am fluent in both Croatian and
-          English and have a passion for creating intuitive and engaging user
-          experiences. My goal is to secure a position as a front-end developer
-          where I can progress and utilize my skills to deliver exceptional
-          results.
+          My name is Vladimir Šimić I am a 36-year-old Croatian living in Bosnia
+          and Herzegovina. I`ve spent 4 years studying a web development. I am
+          fluent in both Croatian and English and have a passion for creating
+          intuitive and engaging user experiences. My goal is to secure a
+          position as a front-end developer where I can progress and utilize my
+          skills to deliver exceptional results.
         </p>
         <p>
           Please note that I do not have commercial experience working as a
