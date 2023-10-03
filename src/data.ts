d77f16200ce453871c99228807b9d83527f2b96e
VladoSimic23 <57799129+VladoSimic23@users.noreply.github.com>
2023-10-03 17:14:06 +0200

    Update data.ts

--- src/data.ts
+++ src/data.ts
@@ -68,7 +68,7 @@ export const projectsData: DataI[] = [
     id: 8,
     title: "Avantgarde Service",
     image:
-      "https://drive.google.com/file/d/1jJQnVzVky1EcdSSNzk4rNkdyZiGK_Fvv/preview",
+      "https://lh3.googleusercontent.com/pw/ADCreHcwsmkomYg6Y3raUhEbgREh_v6UD-HWOK_4lJ9v2DPx27yMZsveDN2lzQH7YwQ3fGid_ucglxvlP8QdFmmTtUXVmJQg74rRDkjfpwaJUUyncrdtyz6fLBgx4Y9L0pclr1_ienl-MlBB7uD1FA54eK5VviI_VWy5ZbevOTLkuZsobFyqJaEoQeTGI6yagKgIw8kI4Vk_OluU8gXHcHUHWJWqDchoDe7c7ulT7Dz73HvyxZoZnvhP7U7x9B4E4Pmq5sdVtYdB0D7BsX8Ei2H5JHsRp6cTLQKUTz6NThMBptdK1_f5DYc15DC-P1Ir3SntDKQVhuTk_RzEgYjpCUzQgB29vpETB8u9ralrEZBo83UxPuP74ACdrnZ-1-vKSg7LwDoqQ6xUw5vgrTxj34hiUvDPMweudIxEwCOoFCHKwAuUp40becnkQsh_QwhYZuy_Gd8l7opmetP1cqeCT064qm2V-9iCA6iPOBwk9b5DAuX_ZG8DP-E3b5wvkJOB4iOdRbDraWJY1f6ha6y7fU1ybZiqq2PjY76c4vectucLV56rJmZLfxla1cmpen1M0YTgVwE4bfpkm760Li8vCoUuj0qyqHM47AQgcQK0eNqqp86tBnQ-eA7O4LPZHQpouhbKiG6WKOLEnshSnIrWseWN6gm3b7eFOfoXJrzMLuN8N_W-USXKa_lS51FWKxrn93gxSrA8DnQPG50bZyD4qZmtYksoN5yPc3jc_h2RiLgmF1AR_S0xNFJaE75Z8hlk0GNG3kyLuRMEjNrBwfP4uM8TRwQLR3psUAlhNU7WEAjFsR04TXmj2DKlVDk9aLLS5urN2eT61ofM5hzaQoZfiA35ZKOD_G3tV9IZi7ShnsQCrVFmf3COCG_U3zfeTp93DmtHgJVReMKGIjFhlSsK4t13vDP8shEbjrg8Yq7TyErAaDkzhFL9tJlg2J0WQhaE6llPvQnqKUiiANHs3cgYsc4x3ZSY7GaaPA=w1894-h958-s-no?authuser=0",
     projectUrl: "https://avantgardeservice.com",
     githubUrl: "https://github.com/VladoSimic23/AvantgardeServiceFrontend",
   },
