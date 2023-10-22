1de31b59ad00a20f18c0e7122d9455139fe051df
VladoSimic23 <vladosimic525@gmail.com>
2023-10-22 12:16:32 +0200

    Changed some images

--- src/data.ts
+++ src/data.ts
@@ -68,14 +68,14 @@ export const projectsData: DataI[] = [
     id: 8,
     title: "Avantgarde Service",
     image:
-      "https://lh3.googleusercontent.com/pw/ADCreHf1j1MigdCTj8p40FESrCvm4eFoowiCiGlbKFBYGAEm7hi-ePoDpvB73bKoNMJuP2aJFFcN9N6xAtGHWXrWgcM2S8ZNeuMsIzuZ-D49_aLSvrNQQo3ah3dn7qQx6MvLZR9e9lemrDxnXpH6HfJ7H3FGrChWMDrl3JFJ1dag5G4FQenSf4idoNbTg8BYGlwTI8d9aqt_f0ag_zQkvpBPeFmhkznZqDer3pf0f-WhLku7VreniKMmEEQD5qbKy618G3tV4mSJOMSjvhkpzfsYMzVkdXTQdX5qM4T48qgCqbG3UPAT21chzdcH2mVnk-wzwPiTy0c9EbAk8TpI36Ok_4UBJJRSeUpLMdGnHe3XriMn4oRsMlH3JIRCljaCDJQUqYtTx8WLKxIcvFHJuZut-Ssx0e9MPwwIlyyNPjTFJ1o2NDuetun2NP5ZsnfwGIpKGQazuD950Lg2saZjkxfTh1cYr1xCsMLRpw53TOm3N-dwwH-NkwWDmJS5CGMmo6XHCWCw2osHogPTne1r9DiOwBrbqfUyQebi8APYfcLnhZ2RzP8eoarecwNPDlepcGQUVon91irSc-A9ZEhnub-Sa3A0tqBd0aWI3QbMAHOKUAzuqBcF1X-VDBzYd-mB0SrYA-H8Ydm799Vk497ZNBHrzVAwwVAfMWB3wooibKlHaDTEHnlSzwKhH3I4Kq0rzb4Ym1XdscgXs1BJWr9rYshhGQkiwjMLOzl0erwsVZ6M5OQJ2iLewwoDqtpPAkYBKbUhqGNXS3fdbx0EXHw3dMaSeP9BC_mEFhphQ2a1VGVPyQ5UUoIBIiQAR0YFesIxzLJY8ZB2se_ezfCy8jtaF69hK8TBGONvoPx1i-OqoI2mXtvrgn_WBj_eptM8XxMO0tMnyEvGE8UPduvyOatI5cZNv7w4T3h6r_SS16zQ1B_jH8FIf4VEQ8XEzTboV9lohjDY5D5Vr8TwyhWYxWc9Xz7WSPcacSJ4kg=w1894-h958-s-no-gm?authuser=0",
+      "https://images2.imgbox.com/32/83/uiAWXWcd_o.png",
     projectUrl: "https://avantgardeservice.com",
     githubUrl: "https://github.com/VladoSimic23/AvantgardeServiceFrontend",
   },
   {
     id: 9,
     title: "Rolar",
-    image: "https://lh3.googleusercontent.com/pw/ADCreHdGP1QiIqixdcLtQXMuCJPT19AjunlU0ezcn-of9EoSlU05Wm59UVC3tEpCD2yN84Oecin23CCIzKNuqOOSMqFgdFcIrDrK5uIeUCyCfRxHEZbHbdnGInPW8vgyETOPxrSYq7l_NUVPKkHKsUvuWA9Jwqn8DO_FyosC05VyQw2pEVNd6RYcrRwRCnFYddJxfnq2bZdOrjZeZ71XzfDMvNYD1tC4kYNWcrOcCQAneUFIeF5p17vdK9rwtrE8-XJl_OWkALSyBspyjT6BYLY7nbFmdz_hhA5jO2BWfEIc-Tjc6gbQeH5_1LvBByff4XUR89jDkZSi7glv600dO2R-rOzbjeUY8pm64GyAOd1R3CbGjx1FbilfV51EwI5MP8WNQED1Pn8FCC3fjIwNasp2hshDr8W38Xgrjkt2JYSVcnZJo0hqlnBTOO8rkmEKw3vpje04-7vEghUYelZC9epjHapWIaoyVjdDWeL0AcD4VM4MIUv6mrzhTjq0o-OdZjrSZrlIkPGpZ9L9pwgcPPc7W7wjs8YtxLEqFp7ZoOrAuaiAEJ-ulYUskRIATdIZJizQaTu3U8msYvuLlrrvZkqzw_0oPFBhNMVHEV9IP6GZVuQzPG4tXPWD473F8pdvsPgsHZURWWsFxecKeNvZMb8fuKe4UKnx_zMuX7xrfXxQPdHa5rSRzlFdPtZBnEGaRHd0GPgeOtj7U55oFE42gz-wUscRpoJWELvG6mw0OEIvif5zAkXfjUVV9citNx-0Fb815oK1XdHALDs122FXtRuPrtkK4pf6iUWbeKBHCu54USk5eY-G0xdv4S03nmEJ3aD4mvGTSx-0WE3ndYUUwwOykkhdH8uRabqAvItkrvT5bFBn4fkO60xARCU6X-TgfUgb-f-WijFjtk7a4tWPRhBzNv7moGYknbMscpRa2daHn1AQdiOxx1S-uzYqJw3J9LSj5Faa_rMU0YRHaJtHegN0vx8FtCbVWg=w1913-h953-s-no-gm?authuser=0",
+    image: "https://images2.imgbox.com/47/93/iAd1TjGh_o.png",
     projectUrl: "https://rolar2023.netlify.app/",
     githubUrl: "https://github.com/VladoSimic23/Rolar2023"
   }
